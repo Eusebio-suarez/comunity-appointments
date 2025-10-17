d4c20a2ff32cacb848cc8f61a7ccd8b9abbe5848
Eusebio-suarez <eusebiosuaresmartines@gmail.com>
2025-10-17 09:04:03 -0500

    endpoind para registar usuarios

---
+++ src/main/java/com/cumunity_appointments/controllers/AuthController.java
@@ -0,0 +1,39 @@
+package com.cumunity_appointments.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cumunity_appointments.dto.request.UserRequestDTO;
+import com.cumunity_appointments.dto.response.UserResponseDTO;
+import com.cumunity_appointments.services.UserService;
+import com.cumunity_appointments.utils.ApiResponse;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/api/v1/users/auth")
+public class AuthController {
+    
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    private ResponseEntity<ApiResponse<?>> register(@Valid @RequestBody UserRequestDTO userRequestDTO){
+
+        UserResponseDTO user = userService.register(userRequestDTO);
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+            .body(ApiResponse.builder()
+                .success(true)
+                .message("exito")
+                .data(user)
+                .build()
+            );
+    }
+
+}
