38f91518f6560886f32bb4d9fa91bceb3236e487
Eusebio-suarez <eusebiosuaresmartines@gmail.com>
2025-10-17 06:28:02 -0500

    configuracion inicial

---
+++ src/main/java/com/cumunity_appointments/CumunityAppointmentsApplication.java
@@ -0,0 +1,13 @@
+package com.cumunity_appointments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CumunityAppointmentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CumunityAppointmentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cumunity_appointments/CumunityAppointmentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cumunity_appointments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CumunityAppointmentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
